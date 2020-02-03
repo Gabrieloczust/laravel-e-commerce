67587456dff473e281e5b5b21ce7780d1726c0f4
Gabriel Oczust <gabriel.oczust@gmail.com>
2020-02-02 21:06:42 -0300

    Factory and Seeder of Sale

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,8 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            CategorySeeder::class,
-            ProductSeeder::class
+            //CategorySeeder::class,
+            ProductSeeder::class,
+            SaleSeeder::class,
         ]);
     }
 }
